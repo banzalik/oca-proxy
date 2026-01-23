7b724111f4e9b2ddd2cc49152f3ace915b741bfb
Oleksandr Boichenko <banzalik@gmail.com>
2026-01-22 19:27:10 -0800

    Remove deprecated Husky shim and bump to 9.1.7
    - Stop sourcing husky.sh in pre-commit/pre-push; run Biome directly
    - Replace .husky/_/husky.sh with deprecation notice for v10
    - Update lockfile

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,41 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-# Husky - Git hooks manager shim
-# Initializes environment and re-executes the hook script.
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-if [ -z "${husky_skip_init-}" ]; then
-  husky_debug() {
-    [ "${HUSKY_DEBUG:-}" = "1" ] && printf '%s\n' "husky (debug) - $1"
-  }
-
-  hook_name="$(basename -- "$0")"
-  readonly hook_name
-
-  husky_debug "starting $hook_name..."
-
-  if [ "${HUSKY-}" = "0" ]; then
-    husky_debug "HUSKY env variable is set to 0, skipping hook"
-    exit 0
-  fi
-
-  if [ -f "${HOME}/.huskyrc" ]; then
-    husky_debug "sourcing ~/.huskyrc"
-    # shellcheck disable=SC1090
-    . "${HOME}/.huskyrc"
-  fi
-
-  husky_skip_init=1
-  readonly husky_skip_init
-  export husky_skip_init
-
-  # Re-run the current hook script with husky initialized.
-  sh -e "$0" "$@"
-  husky_exit_code="$?"
-
-  if [ "$husky_exit_code" = "0" ]; then
-    husky_debug "$hook_name completed"
-  else
-    printf '%s\n' "husky - $hook_name hook exited with code $husky_exit_code (error)"
-  fi
-
-  exit "$husky_exit_code"
-fi
+They WILL FAIL in v10.0.0
+"
